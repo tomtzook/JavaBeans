13c112839d8bca24213bcd56b323b900e5650f20
Tom Tzook <tomtzook@gmail.com>
2018-08-25 00:09:16 +0300

    added javadoc to atomic observable implementations

--- src/main/java/com/beans/observables/properties/concurrent/AtomicObservableBooleanProperty.java
+++ src/main/java/com/beans/observables/properties/concurrent/AtomicObservableBooleanProperty.java
@@ -2,6 +2,25 @@ package com.beans.observables.properties.concurrent;
 
 import com.beans.observables.properties.ObservableBooleanProperty;
 
+/**
+ * <p>
+ *     A <b>thread-safe</b> implementation of {@link ObservableBooleanProperty}, holding a
+ *     variable which is accessed for writing or reading through {@link #setAsBoolean(boolean)}
+ *     and {@link #getAsBoolean()}.
+ * </p>
+ * <p>
+ *     This implementation relays on the fact that the Java Language Specifications guarantee that an access (read/write)
+ *     to a <em>volatile</em> field is atomic, and will be visible to all threads. Using this, reading
+ *     is guaranteed as atomic.
+ * </p>
+ * <p>
+ *     Writing to this property holds a lock, preventing additional write. Invocation
+ *     of added listener is done within the lock. This locking doesn't prevent reading
+ *     of the updated value.
+ * </p>
+ *
+ * @since JavaBeans 1.0
+ */
 public class AtomicObservableBooleanProperty extends ObservableBooleanProperty {
 
     private volatile boolean mValue;
@@ -11,6 +30,9 @@ public class AtomicObservableBooleanProperty extends ObservableBooleanProperty {
         mValue = initialValue;
     }
 
+    /**
+     * Initializes the property with a value of <em>false</em>.
+     */
     public AtomicObservableBooleanProperty() {
         this(false);
     }
--- src/main/java/com/beans/observables/properties/concurrent/AtomicObservableLongProperty.java
+++ src/main/java/com/beans/observables/properties/concurrent/AtomicObservableLongProperty.java
@@ -2,6 +2,25 @@ package com.beans.observables.properties.concurrent;
 
 import com.beans.observables.properties.ObservableLongProperty;
 
+/**
+ * <p>
+ *     A <b>thread-safe</b> implementation of {@link ObservableLongProperty}, holding a
+ *     variable which is accessed for writing or reading through {@link #setAsLong(long)}
+ *     and {@link #getAsLong()}.
+ * </p>
+ * <p>
+ *     This implementation relays on the fact that the Java Language Specifications guarantee that an access (read/write)
+ *     to a <em>volatile</em> field is atomic, and will be visible to all threads. Using this, reading
+ *     is guaranteed as atomic.
+ * </p>
+ * <p>
+ *     Writing to this property holds a lock, preventing additional write. Invocation
+ *     of added listener is done within the lock. This locking doesn't prevent reading
+ *     of the updated value.
+ * </p>
+ *
+ * @since JavaBeans 1.0
+ */
 public class AtomicObservableLongProperty extends ObservableLongProperty {
 
     private volatile long mValue;
@@ -11,6 +30,9 @@ public class AtomicObservableLongProperty extends ObservableLongProperty {
         mValue = initialValue;
     }
 
+    /**
+     * Initializes the property with a value of <em>0</em>.
+     */
     public AtomicObservableLongProperty() {
         this(0);
     }
