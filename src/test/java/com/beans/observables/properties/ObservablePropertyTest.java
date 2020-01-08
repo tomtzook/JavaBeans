2401ee51d6e6416fff5ecb7e0c6125be571fc99f
Tom Tzook <tomtzook@gmail.com>
2020-01-09 01:24:53 +0200

    junit5

--- src/test/java/com/beans/observables/properties/ObservablePropertyTest.java
+++ src/test/java/com/beans/observables/properties/ObservablePropertyTest.java
@@ -1,7 +1,7 @@
 package com.beans.observables.properties;
 
 import com.beans.observables.listeners.ChangeListener;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
